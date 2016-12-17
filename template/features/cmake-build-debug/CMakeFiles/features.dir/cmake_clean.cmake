4d755710a7151d873c547069fed14161d1b90973
ajit dias <ajitdias@ajits-MacBook.local>
2016-12-17 13:40:58 -0500

    more stuff

---
+++ template/features/cmake-build-debug/CMakeFiles/features.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/features.dir/main.cpp.o"
+  "features.pdb"
+  "features"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/features.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
